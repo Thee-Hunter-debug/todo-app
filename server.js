6dd39d6be8cd8846238f2e2a56f348f99e592385
Thee-Hunter-debug <rremokgalagadi@gmail.com>
2026-07-11 10:00:28 +0200

    Update db

--- server.js
+++ server.js
@@ -7,17 +7,26 @@ const { Pool } = require("pg");
 const crypto = require("crypto");
 const token = crypto.randomBytes(32).toString("hex");
 const nodemailer = require("nodemailer");
+const { neon } = require('@neondatabase/serverless');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const pool = new Pool({
+pool.connect()
+  .then(() => console.log("Connected to PostgreSQL"))
+  .catch(err => {
+    console.error("Database connection failed:", err);
+    process.exit(1);
+  });
+
+/*onst pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
+*/
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
